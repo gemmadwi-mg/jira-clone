fa7ec2999efc1650baf9e7a997807fb4356cdba8
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-12-03 18:25:20 +0700

    building a session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return ( 
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return (
     <div>
-      <Button>Click me</Button>
+      Only visible to authorize user
+      <Button onClick={() => mutate()}>Log out</Button>
     </div>
-  )
+  );
 }
