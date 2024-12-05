bc078b11313c20c35b6bcea86e2d8e6b65a22007
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-12-06 04:08:21 +0700

    create workspace switcher

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -5,7 +5,22 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { ID } from "node-appwrite";
 
-const app = new Hono().post(
+const app = new Hono()
+.get(
+  "/",
+  sessionMiddleware,
+  async (c) => {
+    const databases = c.get("databases");
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACES_ID,
+    )
+
+    return c.json({ data: workspaces })
+  }
+)
+.post(
   "/",
   zValidator("form", createWorkspaceSchema),
   sessionMiddleware,
