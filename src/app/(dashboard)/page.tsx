013496171e0fe9713c113b7539b117c8ca8117dd
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-12-14 01:48:05 +0700

    building a responsive modal

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { getCurrent } from "@/features/auth/actions";
-import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+import { getWorkspaces } from "@/features/workspaces/actions";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -7,9 +7,10 @@ export default async function Home() {
 
   if (!user) redirect("/sign-in");
 
-  return (
-    <div className="bg-neutral-500 p-4">
-      <CreateWorkspaceForm />
-    </div>
-  );
+  const workspaces = await getWorkspaces();
+  if (workspaces.total === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workpaces/${workspaces?.documents[0].$id}`);
+  }
 }
